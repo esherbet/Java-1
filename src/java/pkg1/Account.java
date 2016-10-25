0b75b621e7c0373baa50f563053e0f46e090e581
w007ehs <w007ehs@LAB9C8E99F37D60.wright.edu>
2016-10-25 16:14:01 -0400

    added private attributes

---
+++ src/java/pkg1/Account.java
@@ -0,0 +1,24 @@
+package java.pkg1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Account Object 
+ * @author Evan Sherbet 
+ */
+public class Account {
+    //private attributes
+    private long accNum; //account#
+    private String accName; //account holder
+    private char accType; // c:hecking or s:aving
+    private double minBal;
+    private float accFees;
+    private char feeFreq; //M:onthly, Q:uarterly, etc
+    private float intrate;
+    private char intfreq; //M:onthly, Q:uarterly, etc
+    private double accBal;
+}
